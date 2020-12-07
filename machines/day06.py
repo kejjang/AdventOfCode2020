45f55a068f50466990bd3e78ce49049ed8f48cae
Kej <kejjang@gmail.com>
2020-12-07 13:57:03 +0800

    Day 06 - fix parameter

--- machines/day06.py
+++ machines/day06.py
@@ -5,15 +5,15 @@ class Operator(Base):
     def exec(self, part: int = 1):
         return (parts := {1: self.__part1, 2: self.__part2}).get(part if part in parts else 1)(self.data)
 
-    def __part1(self, b_pass_all):
-        groups = " ".join(self.data).split("  ")
+    def __part1(self, questions):
+        groups = " ".join(questions).split("  ")
         count = 0
         for g in groups:
             count += len(set(g.replace(" ", "")))
         return count
 
-    def __part2(self, b_pass_all):
-        groups = " ".join(self.data).split("  ")
+    def __part2(self, questions):
+        groups = " ".join(questions).split("  ")
         count = 0
         for g in groups:
             n = len(g.split(" "))  # number of people in this group
