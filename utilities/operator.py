edd1f56f1c718f35044c1c72b208a2adc9390aec
Kej <kejjang@gmail.com>
2020-12-25 17:59:45 +0800

    fix stupid mistakes...

--- utilities/operator.py
+++ utilities/operator.py
@@ -3,11 +3,11 @@ from utilities.data import PuzzleInput
 
 class Base:
     def __init__(self, day_num: int = -1, to_int=False):
-        if day_num in range(1, 25):
+        if day_num in range(1, 26):
             self.load_data(day_num, to_int)
 
     def load_data(self, day_num: int = -1, to_int=False):
-        if day_num in range(1, 25):
+        if day_num in range(1, 26):
             self.data = PuzzleInput.get(day_num)
             if to_int:
                 self.data = [int(item) for item in self.data]
